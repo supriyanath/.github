8eb97425fbb4bd22d3281672cb5f53876c0756c2
supriyanath <34536888+supriyanath@users.noreply.github.com>
2024-01-28 20:31:03 +0530

    sundari

--- Caled.py
+++ Caled.py
@@ -5,3 +5,4 @@ month = int(input ("Please enter the month: "))    # Here, it will take the mont
   
 # Now, we will display the calendar  
 Print ("The Calendar of: ", calendar.month(year, month))  
+Print ("The Calendar of: ", calendar.month(year, month))
